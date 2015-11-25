fe76249e7273e4967713f8fc0e5f3dc6ffbebda7
Vojtěch Kadlec <venku.mrzneazprasti@email.cz>
2015-11-25 10:03:03 +0100

    Space slider added.

--- puzzle.js
+++ puzzle.js
@@ -2,7 +2,8 @@
     // the accepted properties for your application
     var properties = [
       {type: 'range', id: "X Count", value: 4, min: 2, max: 20, step: 1},
-      {type: 'range', id: "Y Count", value: 4, min: 2, max: 20, step: 1}
+      {type: 'range', id: "Y Count", value: 4, min: 2, max: 20, step: 1},
+      {type: 'range', id: "Space", value: -1, min: -1, max: 100, step: 1}
     ];
     function getPathData(type,xOffset,yOffset,rotation){
         rotation = rotation*90;
@@ -11,7 +12,7 @@
         switch(type){
             case 1:
                 xPlace = 11+xOffset;
-                yPlace = 814-yOffset+hglob-800;            
+                yPlace = 814-yOffset+hglob-800;
                 return '<path fill="none" stroke="#000000" stroke-miterlimit="10" d="M'+xPlace+','+yPlace+' h 200 v -48.59 c 0 -37.5 13.463 -26.367 13.463 -26.367 c 4.745 3.142 10.422 4.98 16.537 4.98 c 16.569 0 30 -13.433 30 -30.002 c 0 -16.568 -13.431 -30.002 -30 -30.002 c -6.112 0 -11.792 1.839 -16.534 4.98 c 0 0 -13.466 11.133 -13.466 -26.367 v -48.632 h -47.917 c -37.5 0 -26.367 13.466 -26.367 13.466 c 3.14 4.741 4.978 10.422 4.978 16.531 c 0 16.568 -13.43 30.002 -30 30.002 c -16.569 0 -30 -13.434 -30 -30.002 c 0 -6.114 1.839 -11.79 4.979 -16.537 c 0 0 11.133 -13.46 -26.367 -13.46 c -0.236 0 -0.461 0 -0.694 0 h -48.612 v 200 Z" transform="rotate('+rotation+','+(xPlace+100)+','+(yPlace-100)+')"/>';
             case 2:
                 xPlace = 241+xOffset-200;
@@ -40,15 +41,18 @@
       var params = args[0];
       var xCount = params['X Count'];
       var yCount = params['Y Count']; 
+      var space = params['Space'];
       var scale = 1;
       
-      var gap = 80;
+      var gap=(50+space);
+      if(space<0) gap=0;
+      
         var width = (774)/scale;
         var height = (774)/scale;
-        hglob = (yCount*200);
+        hglob = (yCount*(200+gap));
         if ((xCount > 1) && (yCount > 1) && (scale > 0)){
         
-            var svgheader ='<?xml version="1.0" encoding="utf-8"?><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="'+width+'px" height="'+height+'px" viewBox="0 0 '+((xCount*200)+25)+' '+((yCount*200)+28)+'" xml:space="preserve">';
+            var svgheader ='<?xml version="1.0" encoding="utf-8"?><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="'+width+'px" height="'+height+'px" viewBox="0 0 '+((xCount*(200+gap))+25)+' '+((yCount*(200+gap))+28)+'" xml:space="preserve">';
             var svgend = '</svg>';
             
             var svgOut = svgheader;
@@ -56,45 +60,45 @@
             //assembly corners by right peace
             svgOut=svgOut.concat(getPathData(1,0,0,0));
             if(((xCount%2)==0)&&((yCount%2)==0)){
-                svgOut=svgOut.concat(getPathData(1,(xCount-1)*200,(yCount-1)*200,2));
-                svgOut=svgOut.concat(getPathData(1,(xCount-1)*200,0,3));
-                svgOut=svgOut.concat(getPathData(1,0,(yCount-1)*200,1));
+                svgOut=svgOut.concat(getPathData(1,(xCount-1)*(200+gap),(yCount-1)*(200+gap),2));
+                svgOut=svgOut.concat(getPathData(1,(xCount-1)*(200+gap),0,3));
+                svgOut=svgOut.concat(getPathData(1,0,(yCount-1)*(200+gap),1));
             }else if((xCount%2)==0){
-                svgOut=svgOut.concat(getPathData(1,(xCount-1)*200,0,3));
-                svgOut=svgOut.concat(getPathData(5,(xCount-1)*200,(yCount-1)*200,2));
-                svgOut=svgOut.concat(getPathData(5,0,(yCount-1)*200,1));
+                svgOut=svgOut.concat(getPathData(1,(xCount-1)*(200+gap),0,3));
+                svgOut=svgOut.concat(getPathData(5,(xCount-1)*(200+gap),(yCount-1)*(200+gap),2));
+                svgOut=svgOut.concat(getPathData(5,0,(yCount-1)*(200+gap),1));
             }else if((yCount%2)==0){
-                svgOut=svgOut.concat(getPathData(1,0,(yCount-1)*200,1));
-                svgOut=svgOut.concat(getPathData(5,(xCount-1)*200,0,3));
-                svgOut=svgOut.concat(getPathData(5,(xCount-1)*200,(yCount-1)*200,2));
+                svgOut=svgOut.concat(getPathData(1,0,(yCount-1)*(200+gap),1));
+                svgOut=svgOut.concat(getPathData(5,(xCount-1)*(200+gap),0,3));
+                svgOut=svgOut.concat(getPathData(5,(xCount-1)*(200+gap),(yCount-1)*(200+gap),2));
             }else{
-                svgOut=svgOut.concat(getPathData(1,(xCount-1)*200,(yCount-1)*200,2));
-                svgOut=svgOut.concat(getPathData(5,0,(yCount-1)*200,1));
-                svgOut=svgOut.concat(getPathData(5,(xCount-1)*200,0,3));
+                svgOut=svgOut.concat(getPathData(1,(xCount-1)*(200+gap),(yCount-1)*(200+gap),2));
+                svgOut=svgOut.concat(getPathData(5,0,(yCount-1)*(200+gap),1));
+                svgOut=svgOut.concat(getPathData(5,(xCount-1)*(200+gap),0,3));
             }
             
             //bottom side
             for (x=2;x<xCount;x++){
-                svgOut=svgOut.concat(getPathData(2+(x%2),(x-1)*200,0,0));
+                svgOut=svgOut.concat(getPathData(2+(x%2),(x-1)*(200+gap),0,0));
             }
             //left side
             for (y=2;y<yCount;y++){
-                svgOut=svgOut.concat(getPathData(3-(y%2),0,(y-1)*200,1));
+                svgOut=svgOut.concat(getPathData(3-(y%2),0,(y-1)*(200+gap),1));
             }
             //right side
             for (x=2;x<xCount;x++){
-                svgOut=svgOut.concat(getPathData(3-((x+(yCount%2))%2),(x-1)*200,(yCount-1)*200,2));
+                svgOut=svgOut.concat(getPathData(3-((x+(yCount%2))%2),(x-1)*(200+gap),(yCount-1)*(200+gap),2));
             }
             //top side
             for (y=2;y<yCount;y++){
-                svgOut=svgOut.concat(getPathData(2+((y+(xCount%2))%2),(xCount-1)*200,(y-1)*200,3));
+                svgOut=svgOut.concat(getPathData(2+((y+(xCount%2))%2),(xCount-1)*(200+gap),(y-1)*(200+gap),3));
             }
             //fill middle
             for (x=2; x<xCount;x++){
                 var k=0;
                 if((x%2)==0) k=1;
                 for(y=2;y<yCount;y++){
-                    svgOut=svgOut.concat(getPathData(4,(x-1)*200,(y-1)*200,(x+(y%2))%2));
+                    svgOut=svgOut.concat(getPathData(4,(x-1)*(200+gap),(y-1)*(200+gap),(x+(y%2))%2));
                 }
             }
       }
